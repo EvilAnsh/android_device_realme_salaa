1fe0b16d074bf1ae3032ad65c6c2a63195efbb99
Arne Coucheron <arco68@gmail.com>
2021-01-12 05:45:19 +0100

    salaa: Silence clang warnings during kernel compile
    clang-12: warning: argument unused during compilation: '-fuse-ld=lld' [-Wunused-command-line-argument]
    
    Change-Id: I66a629f3573e3fad8aa085ea115475f02d60a7ea

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -66,6 +66,7 @@ TARGET_KERNEL_ARCH := arm64
 TARGET_KERNEL_HEADER_ARCH := arm64
 TARGET_KERNEL_SOURCE := kernel/realme/mt6785
 TARGET_KERNEL_CONFIG := salaa_defconfig
+TARGET_KERNEL_ADDITIONAL_FLAGS := HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
 TARGET_KERNEL_CLANG_COMPILE := true
 TARGET_KERNEL_CLANG_VERSION := proton
 TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/$(TARGET_KERNEL_CLANG_VERSION)-clang
