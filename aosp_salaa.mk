75a0ffad46e2c5f04d4a9880160904456eb6b351
Ansh <singhansh64321@gmail.com>
2023-10-31 17:48:52 +0000

    salaa: Adapt for Elixir!
    Signed-off-by: Ansh <singhansh64321@gmail.com>

--- aosp_salaa.mk
+++ aosp_salaa.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2023 The LineageOS Project
+# Copyright (C) 2023 The Project Elixir
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -12,10 +12,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/non_ab_device.mk)
 # Inherit from device makefile.
 $(call inherit-product, device/realme/salaa/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Elixir stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_salaa
+PRODUCT_NAME := aosp_salaa
 PRODUCT_DEVICE := salaa
 PRODUCT_MANUFACTURER := realme
 PRODUCT_BRAND := realme
@@ -23,12 +23,18 @@ PRODUCT_MODEL := realme 7/Narzo 20 Pro/Narzo 30 4G
 
 PRODUCT_GMS_CLIENTID_BASE := android-realme
 
+# Elixir Stuff.
+ELIXIR_MAINTAINER := ꪖꪀ𝘴ꫝ|邪惡的
+IS_PHONE := true
+TARGET_SUPPORTS_QUICK_TAP := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_INCLUDE_STOCK_ACORE := true
+TARGET_INCLUDE_LIVE_WALLPAPERS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+
 PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=RMX2156L1 \
     PRODUCT_NAME=RMX2156 \
     PRIVATE_BUILD_DESC="sys_mssi_64_cn_armv82-user 12 SP1A.210812.016 1689241820072 release-keys"
 
 BUILD_FINGERPRINT := realme/RMX2156/RMX2156L1:12/SP1A.210812.016/Q.11e8c10-4e353:user/release-keys
-
-# Time
-LINEAGE_VERSION_APPEND_TIME_OF_DAY := true
