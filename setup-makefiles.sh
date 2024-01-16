142054d9968cb3d26974d00eec8eec399ca8527e
Bruno Martins <bgcngm@gmail.com>
2024-01-16 22:09:40 +0000

    salaa: Add firmware extraction support
    Change-Id: I61b0f4cdddf65166cb12a39926e1fcb504241975

--- setup-makefiles.sh
+++ setup-makefiles.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
 # Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2020 The LineageOS Project
+# Copyright (C) 2017-2025 The LineageOS Project
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -74,6 +74,9 @@ setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
 write_headers
 
 write_makefiles "${MY_DIR}/proprietary-files.txt" true
+
+# Append the calls to firmware images
+append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
 
 # Finish
 write_footers
