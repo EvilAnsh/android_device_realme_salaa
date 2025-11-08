ebab35c788fbc7b1fd22ebc02be1c27a06eb0e54
Mashopy <eliasgheeraert@gmail.com>
2025-11-08 18:19:55 +0100

    salaa: Build MediaTek WiFi lib and enable OUI support on wpa_supplicant_8
    Change-Id: I0ff725563a3342b71afd82cc7524befda39ac629
    Signed-off-by: Ansh <singhansh64321@gmail.com>

--- device.mk
+++ device.mk
@@ -328,6 +328,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/lineage/interfaces/power-libperfmgr \
     hardware/mediatek \
     hardware/mediatek/libmtkperf_client \
+    hardware/mediatek/wlan/wifi_hal \
     hardware/oplus
 
 # Thermal
@@ -339,10 +340,11 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.mediatek
 
 # Wi-Fi
+$(call soong_config_set,wpa_supplicant_8,board_wlan_mediatek_stability,true)
+
 PRODUCT_PACKAGES += \
     android.hardware.wifi-service \
     hostapd \
-    libwifi-hal-wrapper \
     wpa_supplicant
 
 PRODUCT_COPY_FILES += \
