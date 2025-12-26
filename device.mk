740e4389743d19d4b4094d90fe1f621a4d56f91e
Debayan Kar <kardebayan3@gmail.com>
2025-12-26 14:33:31 +0000

    salaa: Drop doze
    Change-Id: I6868dede0e5813419109777fe59dff137cb7a72e
    Signed-off-by: Ansh <singhansh64321@gmail.com>

--- device.mk
+++ device.mk
@@ -70,10 +70,6 @@ TARGET_SCREEN_WIDTH := 1080
 PRODUCT_PACKAGES += \
     libcamera2ndk_vendor
 
-# Doze
-PRODUCT_PACKAGES += \
-    OplusDoze
-
 # ConfigStore
 PRODUCT_PACKAGES += \
     disable_configstore
