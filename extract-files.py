EvilAnsh/android_device_realme_salaa

commit 05fcb06609b4bbbce2765622dc4bffdb29de0d6c

salaa: Migrate to py extract utils
Change-Id: I5664bb9bb81ce308673261f239a806c1cbfcd7ea

extract-files.py
@@ -1,0 +1,104 @@
+#!/usr/bin/env -S PYTHONPATH=../../../tools/extract-utils python3
+#
+# SPDX-FileCopyrightText: 2024 The LineageOS Project
+# SPDX-License-Identifier: Apache-2.0
+#
+
+from extract_utils.file import File
+from extract_utils.fixups_blob import (
+    BlobFixupCtx,
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixup_remove,
+    lib_fixups,
+    lib_fixups_user_type,
+)
+from extract_utils.main import (
+    ExtractUtils,
+    ExtractUtilsModule,
+)
+from extract_utils.tools import (
+    llvm_objdump_path,
+)
+from extract_utils.utils import (
+    run_cmd,
+)
+
+namespace_imports = [
+    'device/realme/salaa',
+	'hardware/mediatek',
+	'hardware/mediatek/libmtkperf_client',
+	'hardware/oplus',
+]
+
+
+def lib_fixup_odm_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'odm' else None
+
+
+def lib_fixup_vendor_suffix(lib: str, partition: str, *args, **kwargs):
+    return f'{lib}_{partition}' if partition == 'vendor' else None
+
+
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+    (
+        'libhwm-oplus',
+        'libremosaic_wrapper',
+        'libremosaiclib',
+        'vendor.oplus.hardware.biometrics.fingerprint@2.1',
+        'vendor.oplus.hardware.commondcs@1.0',
+    ): lib_fixup_odm_suffix,
+    (
+        'vendor.mediatek.hardware.videotelephony@1.0',
+    ): lib_fixup_vendor_suffix,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    ('odm/bin/hw/vendor.oplus.hardware.charger@1.0-service','vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron',
+     'vendor/lib/libnvram.so', 'vendor/lib/libsysenv.so', 'vendor/lib64/libnvram.so', 'vendor/lib64/libsysenv.so'): blob_fixup()
+        .add_needed('libbase_shim.so'),
+    'system_ext/lib64/libimsma.so': blob_fixup()
+        .replace_needed('libsink.so', 'libsink-mtk.so'),
+    'system_ext/lib64/libsource.so': blob_fixup()
+        .add_needed('libui_shim.so'),
+    ('vendor/bin/hw/android.hardware.gnss-service.mediatek', 'vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so'): blob_fixup()
+        .replace_needed('android.hardware.gnss-V1-ndk_platform.so', 'android.hardware.gnss-V1-ndk.so'),
+    'vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b': blob_fixup()
+        .add_needed('libstagefright_foundation-v33.so')
+        .replace_needed('libavservices_minijail_vendor.so', 'libavservices_minijail.so'),
+    'vendor/bin/hw/mtkfusionrild': blob_fixup()
+        .add_needed('libutils-v32.so'),
+    ('vendor/bin/mnld', 'vendor/lib/libcam.utils.sensorprovider.so', 'vendor/lib/libaalservice.so', 'vendor/lib64/hw/android.hardware.sensors@2.X-subhal-mediatek.so', 'vendor/lib64/libcam.utils.sensorprovider.so', 'vendor/lib64/libaalservice.so'): blob_fixup()
+        .add_needed('android.hardware.sensors@1.0-convert-shared.so'),
+    'vendor/etc/init/android.hardware.media.c2@1.2-mediatek.rc': blob_fixup()
+        .regex_replace('@1.2-mediatek', '@1.2-mediatek-64b'),
+    'vendor/lib/hw/audio.primary.mt6785.so': blob_fixup()
+        .replace_needed('libalsautils.so','libalsautils-v31.so'),
+    'vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so': blob_fixup()
+        .replace_needed('libutils.so', 'libutils-v32.so')
+        .add_needed('libcamera_metadata_shim.so'),
+    'vendor/lib64/libmnl.so': blob_fixup()
+        .add_needed('libcutils.so'),
+    'vendor/lib64/libmtkcam_featurepolicy.so': blob_fixup()
+        .binary_regex_replace(b'\x34\xE8\x87\x40\xB9', b'\x34\x28\x02\x80\x52'),
+    ('vendor/lib64/lib3a.flash.so', 'vendor/lib64/libSQLiteModule_VER_ALL.so'): blob_fixup()
+         .add_needed('liblog.so'),
+    ('vendor/lib/hw/android.hardware.thermal@2.0-impl.so', 'vendor/lib/hw/vendor.mediatek.hardware.pq@2.13-impl.so', 'vendor/lib/libmtkcam_stdutils.so', 'vendor/lib64/hw/android.hardware.thermal@2.0-impl.so', 'vendor/lib64/hw/vendor.mediatek.hardware.pq@2.13-impl.so', 'vendor/lib64/libmtkcam_stdutils.so'): blob_fixup()
+        .replace_needed('libutils.so', 'libutils-v32.so')
+}  # fmt: skip
+
+module = ExtractUtilsModule(
+    'salaa',
+    'realme',
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
+    namespace_imports=namespace_imports,
+    add_firmware_proprietary_file=True,
+)
+
+if __name__ == '__main__':
+    utils = ExtractUtils.device(module)
+    utils.run()

extract-files.sh
@@ -1,130 +1,0 @@
-#!/bin/bash
-#
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2025 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-set -e
-
-DEVICE=salaa
-VENDOR=realme
-
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
-
-ANDROID_ROOT="${MY_DIR}/../../.."
-
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
-    exit 1
-fi
-source "${HELPER}"
-
-# Default to sanitizing the vendor folder before extraction
-CLEAN_VENDOR=true
-
-ONLY_FIRMWARE=
-KANG=
-SECTION=
-
-while [ "${#}" -gt 0 ]; do
-    case "${1}" in
-        -n | --no-cleanup )
-                CLEAN_VENDOR=false
-                ;;
-        --only-firmware )
-                ONLY_FIRMWARE=true
-                ;;        
-        -k | --kang )
-                KANG="--kang"
-                ;;
-        -s | --section )
-                SECTION="${2}"; shift
-                CLEAN_VENDOR=false
-                ;;
-        * )
-                SRC="${1}"
-                ;;
-    esac
-    shift
-done
-
-function blob_fixup {
-    case "$1" in
-        system_ext/lib64/libimsma.so)
-            [ "$2" = "" ] && return 0
-            "${PATCHELF}" --replace-needed "libsink.so" "libsink-mtk.so" "${2}"
-            ;;
-        system_ext/lib64/libsource.so)
-            grep -q libui_shim.so "$2" || "$PATCHELF" --add-needed libui_shim.so "$2"
-            ;;
-        vendor/bin/hw/android.hardware.gnss-service.mediatek|vendor/lib64/hw/android.hardware.gnss-impl-mediatek.so)
-            "${PATCHELF}" --replace-needed "android.hardware.gnss-V1-ndk_platform.so" "android.hardware.gnss-V1-ndk.so" "${2}"
-            ;;
-        vendor/bin/hw/android.hardware.media.c2@1.2-mediatek-64b)
-            "${PATCHELF}" --replace-needed "libavservices_minijail_vendor.so" "libavservices_minijail.so" "${2}"
-            "${PATCHELF}" --add-needed "libstagefright_foundation-v33.so" "${2}"
-            ;;
-        vendor/bin/hw/mtkfusionrild)
-            [ "$2" = "" ] && return 0
-            "${PATCHELF}" --add-needed "libutils-v32.so" "${2}"
-            ;;
-        vendor/bin/mnld|vendor/lib*/libcam.utils.sensorprovider.so|vendor/lib*/libaalservice.so|vendor/lib64/hw/android.hardware.sensors@2.X-subhal-mediatek.so)
-            [ "$2" = "" ] && return 0
-            grep -q "android.hardware.sensors@1.0-convert-shared.so" "${2}" || "${PATCHELF}" --add-needed "android.hardware.sensors@1.0-convert-shared.so" "${2}"
-            ;;
-        vendor/etc/init/android.hardware.bluetooth@1.1-service-mediatek.rc)
-            sed -i '/vts/Q' "$2"
-            ;;
-        vendor/etc/init/android.hardware.media.c2@1.2-mediatek.rc)
-            sed -i 's/@1.2-mediatek/@1.2-mediatek-64b/g' "${2}"
-            ;;
-        vendor/lib/hw/audio.primary.mt6785.so)
-             "${PATCHELF}" --replace-needed "libalsautils.so" "libalsautils-v31.so" "${2}"
-             ;;
-        odm/bin/hw/vendor.oplus.hardware.charger@1.0-service|vendor/bin/hw/android.hardware.neuralnetworks@1.3-service-mtk-neuron|vendor/lib*/libnvram.so|vendor/lib*/libsysenv.so)
-            [ "$2" = "" ] && return 0
-            grep -q "libbase_shim.so" "${2}" || "${PATCHELF}" --add-needed "libbase_shim.so" "${2}"
-            ;;
-        vendor/lib64/hw/android.hardware.camera.provider@2.6-impl-mediatek.so)
-            grep -q "libcamera_metadata_shim.so" "${2}" || "${PATCHELF}" --add-needed "libcamera_metadata_shim.so" "${2}"
-            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
-            ;;
-        vendor/lib64/libmnl.so)
-            [ "$2" = "" ] && return 0
-            grep -q "libcutils.so" "${2}" || "${PATCHELF}" --add-needed "libcutils.so" "${2}"
-            ;;
-        vendor/lib64/libmtkcam_featurepolicy.so)
-            # evaluateCaptureConfiguration()
-            sed -i "s/\x34\xE8\x87\x40\xB9/\x34\x28\x02\x80\x52/" "$2"
-            ;;
-        vendor/lib64/libSQLiteModule_VER_ALL.so|vendor/lib64/lib3a.flash.so)
-            [ "$2" = "" ] && return 0
-            grep -q "liblog.so" "${2}" || "${PATCHELF_0_17_2}" --add-needed "liblog.so" "${2}"
-            ;;
-        vendor/lib*/hw/android.hardware.thermal@2.0-impl.so|vendor/lib*/hw/vendor.mediatek.hardware.pq@2.13-impl.so|vendor/lib*/libmtkcam_stdutils.so)
-            "${PATCHELF}" --replace-needed "libutils.so" "libutils-v32.so" "${2}"
-            ;;
-    esac
-}
-
-if [ -z "${SRC}" ]; then
-    SRC="adb"
-fi
-
-# Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}" false "${CLEAN_VENDOR}"
-
-if [ -z "${ONLY_FIRMWARE}" ]; then
-extract "${MY_DIR}/proprietary-files.txt" "${SRC}" "${KANG}" --section "${SECTION}"
-fi
-
-if [ -z "${SECTION}" ]; then
-    extract_firmware "${MY_DIR}/proprietary-firmware.txt" "${SRC}"
-fi
-
-"${MY_DIR}/setup-makefiles.sh"

setup-makefiles.py
@@ -1,0 +1,1 @@
+#!./extract-files.py --regenerate_makefiles

setup-makefiles.sh
@@ -1,82 +1,0 @@
-#!/bin/bash
-#
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2025 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-set -e
-
-DEVICE=salaa
-VENDOR=realme
-
-# Load extract_utils and do some sanity checks
-MY_DIR="${BASH_SOURCE%/*}"
-if [[ ! -d "${MY_DIR}" ]]; then MY_DIR="${PWD}"; fi
-
-ANDROID_ROOT="${MY_DIR}/../../.."
-
-HELPER="${ANDROID_ROOT}/tools/extract-utils/extract_utils.sh"
-if [ ! -f "${HELPER}" ]; then
-    echo "Unable to find helper script at ${HELPER}"
-    exit 1
-fi
-source "${HELPER}"
-
-function vendor_imports() {
-    cat <<EOF >>"$1"
-		"device/realme/salaa",
-		"hardware/mediatek",
-		"hardware/mediatek/libmtkperf_client",
-		"hardware/oplus",
-EOF
-}
-
-function lib_to_package_fixup_vendor_variants() {
-    if [ "$2" != "vendor" ]; then
-        return 1
-    fi
-    case "$1" in
-        vendor.mediatek.hardware.videotelephony@1.0)
-            echo "${1}_vendor"
-            ;;
-        libhwm-oplus)
-            echo "${1}_odm"
-            ;;
-        libremosaic_wrapper)
-            echo "${1}_odm"
-            ;;
-        libremosaiclib)
-            echo "${1}_odm"
-            ;;
-        vendor.oplus.hardware.biometrics.fingerprint@2.1)
-            echo "${1}_odm"
-            ;;
-        vendor.oplus.hardware.commondcs@1.0)
-            echo "${1}_odm"
-            ;;
-        *)
-            return 1
-            ;;
-    esac
-}
-function lib_to_package_fixup() {
-    lib_to_package_fixup_clang_rt_ubsan_standalone "$1" ||
-        lib_to_package_fixup_proto_3_9_1 "$1" ||
-        lib_to_package_fixup_vendor_variants "$@"
-}
-
-# Initialize the helper
-setup_vendor "${DEVICE}" "${VENDOR}" "${ANDROID_ROOT}"
-
-# Warning headers and guards
-write_headers
-
-write_makefiles "${MY_DIR}/proprietary-files.txt" true
-
-# Append the calls to firmware images
-append_firmware_calls_to_makefiles "${MY_DIR}/proprietary-firmware.txt"
-
-# Finish
-write_footers

update-sha1sums.py
@@ -1,79 +1,0 @@
-#!/usr/bin/env python
-#
-# Copyright (C) 2016 The CyanogenMod Project
-# Copyright (C) 2017-2018 The LineageOS Project
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#      http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-from hashlib import sha1
-import sys
-
-device='salaa'
-vendor='realme'
-
-lines = [ line for line in open('proprietary-files.txt', 'r') ]
-vendorPath = '../../../vendor/' + vendor + '/' + device + '/proprietary'
-needSHA1 = False
-
-def cleanup():
-  for index, line in enumerate(lines):
-    # Remove '\n' character
-    line = line[:-1]
-
-    # Skip empty or commented lines
-    if len(line) == 0 or line[0] == '#':
-      continue
-
-    # Drop SHA1 hash, if existing
-    if '|' in line:
-      line = line.split('|')[0]
-      lines[index] = '%s\n' % (line)
-
-def update():
-  for index, line in enumerate(lines):
-    # Remove '\n' character
-    line = line[:-1]
-
-    # Skip empty lines
-    if len(line) == 0:
-      continue
-
-    # Check if we need to set SHA1 hash for the next files
-    if line[0] == '#':
-      needSHA1 = (' - from' in line)
-      continue
-
-    if needSHA1:
-      # Remove existing SHA1 hash
-      line = line.split('|')[0]
-      filePath = line.split(':')[1] if len(line.split(':')) == 2 else line
-
-      if filePath[0] == '-':
-        file = open('%s/%s' % (vendorPath, filePath[1:]), 'rb').read()
-      else:
-        file = open('%s/%s' % (vendorPath, filePath), 'rb').read()
-
-      hash = sha1(file).hexdigest()
-      lines[index] = '%s|%s\n' % (line, hash)
-
-if len(sys.argv) == 2 and sys.argv[1] == '-c':
-  cleanup()
-else:
-  update()
-
-with open('proprietary-files.txt', 'w') as file:
-  for line in lines:
-    file.write(line)
-
-  file.close()

commit 9857c63d8bbc7c2a6623cec474ba510fb85bb0a3

salaa: Reformat `extract-files.py` and `releasetools.py`
ruff check --config ../../../tools/extract-utils/ruff.toml *.py --fix

ruff format --config ../../../tools/extract-utils/ruff.toml *.py

- Nuked reorder-libs.py too, Fixes: 05fcb06609b4bbbce2765622dc4bffdb29de0d6c

Change-Id: Ife6be491001d4835042fe110b6ea98a02a218107

extract-files.py
@@ -4,14 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
-from extract_utils.file import File
 from extract_utils.fixups_blob import (
-    BlobFixupCtx,
     blob_fixup,
     blob_fixups_user_type,
 )
 from extract_utils.fixups_lib import (
-    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
 )
@@ -19,18 +16,12 @@ from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
 )
-from extract_utils.tools import (
-    llvm_objdump_path,
-)
-from extract_utils.utils import (
-    run_cmd,
-)
 
 namespace_imports = [
     'device/realme/salaa',
-	'hardware/mediatek',
-	'hardware/mediatek/libmtkperf_client',
-	'hardware/oplus',
+    'hardware/mediatek',
+    'hardware/mediatek/libmtkperf_client',
+    'hardware/oplus',
 ]
 
 
@@ -51,9 +42,7 @@ lib_fixups: lib_fixups_user_type = {
         'vendor.oplus.hardware.biometrics.fingerprint@2.1',
         'vendor.oplus.hardware.commondcs@1.0',
     ): lib_fixup_odm_suffix,
-    (
-        'vendor.mediatek.hardware.videotelephony@1.0',
-    ): lib_fixup_vendor_suffix,
+    ('vendor.mediatek.hardware.videotelephony@1.0',): lib_fixup_vendor_suffix,
 }
 
 blob_fixups: blob_fixups_user_type = {

releasetools.py
@@ -6,75 +6,101 @@
 
 import common
 
+
 def FullOTA_InstallBegin(info):
-  data = info.input_zip.read("RADIO/dynamic-remove-oppo")
-  common.ZipWriteStr(info.output_zip, "dynamic-remove-oppo", data)
-  info.script.AppendExtra('update_dynamic_partitions(package_extract_file("dynamic-remove-oppo"));')
-  return
+    data = info.input_zip.read('RADIO/dynamic-remove-oppo')
+    common.ZipWriteStr(info.output_zip, 'dynamic-remove-oppo', data)
+    info.script.AppendExtra(
+        'update_dynamic_partitions(package_extract_file("dynamic-remove-oppo"));'
+    )
+    return
+
 
 def FullOTA_InstallEnd(info):
-  OTA_InstallEnd(info, False)
+    OTA_InstallEnd(info, False)
+
 
 def IncrementalOTA_InstallEnd(info):
-  OTA_InstallEnd(info, True)
+    OTA_InstallEnd(info, True)
+
 
 def AddImageOnly(info, basename, incremental, firmware):
-  if incremental:
-    input_zip = info.source_zip
-  else:
-    input_zip = info.input_zip
-  if firmware:
-    data = input_zip.read("RADIO/" + basename)
-  else:
-    data = input_zip.read("IMAGES/" + basename)
-  common.ZipWriteStr(info.output_zip, basename, data)
+    if incremental:
+        input_zip = info.source_zip
+    else:
+        input_zip = info.input_zip
+    if firmware:
+        data = input_zip.read('RADIO/' + basename)
+    else:
+        data = input_zip.read('IMAGES/' + basename)
+    common.ZipWriteStr(info.output_zip, basename, data)
+
 
 def AddImage(info, basename, dest, incremental):
-  AddImageOnly(info, basename, incremental, False)
-  info.script.Print("Patching {} image unconditionally...".format(dest.split('/')[-1]))
-  info.script.AppendExtra('package_extract_file("%s", "%s");' % (basename, dest))
+    AddImageOnly(info, basename, incremental, False)
+    info.script.Print(
+        'Patching {} image unconditionally...'.format(dest.split('/')[-1])
+    )
+    info.script.AppendExtra(
+        'package_extract_file("%s", "%s");' % (basename, dest)
+    )
+
 
 def OTA_InstallEnd(info, incremental):
-  AddImage(info, "dtbo.img", "/dev/block/by-name/dtbo", incremental)
-  AddImage(info, "vbmeta.img", "/dev/block/by-name/vbmeta", incremental)
-  AddImage(info, "vbmeta_system.img", "/dev/block/by-name/vbmeta_system", incremental)
-  AddImage(info, "vbmeta_vendor.img", "/dev/block/by-name/vbmeta_vendor", incremental)
+    AddImage(info, 'dtbo.img', '/dev/block/by-name/dtbo', incremental)
+    AddImage(info, 'vbmeta.img', '/dev/block/by-name/vbmeta', incremental)
+    AddImage(
+        info,
+        'vbmeta_system.img',
+        '/dev/block/by-name/vbmeta_system',
+        incremental,
+    )
+    AddImage(
+        info,
+        'vbmeta_vendor.img',
+        '/dev/block/by-name/vbmeta_vendor',
+        incremental,
+    )
 
-  bin_map = {
-      'logo': ['logo']
-      }
+    bin_map = {'logo': ['logo']}
 
-  img_map = {
-      'audio_dsp': ['audio_dsp'],
-      'cam_vpu1': ['cam_vpu1'],
-      'cam_vpu2': ['cam_vpu2'],
-      'cam_vpu3': ['cam_vpu3'],
-      'gz': ['gz1', 'gz2'],
-      'lk': ['lk', 'lk2'],
-      'md1img': ['md1img'],
-      'scp': ['scp1', 'scp2'],
-      'spmfw': ['spmfw'],
-      'sspm': ['sspm_1', 'sspm_2'],
-      'tee': ['tee1', 'tee2']
-      }
+    img_map = {
+        'audio_dsp': ['audio_dsp'],
+        'cam_vpu1': ['cam_vpu1'],
+        'cam_vpu2': ['cam_vpu2'],
+        'cam_vpu3': ['cam_vpu3'],
+        'gz': ['gz1', 'gz2'],
+        'lk': ['lk', 'lk2'],
+        'md1img': ['md1img'],
+        'scp': ['scp1', 'scp2'],
+        'spmfw': ['spmfw'],
+        'sspm': ['sspm_1', 'sspm_2'],
+        'tee': ['tee1', 'tee2'],
+    }
 
-  pl = 'preloader_ufs'
-  pl_part = ['sda', 'sdb']
+    pl = 'preloader_ufs'
+    pl_part = ['sda', 'sdb']
 
-  fw_cmd = 'ui_print("Patching radio images unconditionally...");\n'
+    fw_cmd = 'ui_print("Patching radio images unconditionally...");\n'
 
-  AddImageOnly(info, "{}.img".format(pl), incremental, True)
-  for part in pl_part:
-      fw_cmd += 'package_extract_file("{}.img", "/dev/block/{}");\n'.format(pl, part)
+    AddImageOnly(info, '{}.img'.format(pl), incremental, True)
+    for part in pl_part:
+        fw_cmd += 'package_extract_file("{}.img", "/dev/block/{}");\n'.format(
+            pl, part
+        )
 
-  for img in img_map.keys():
-    AddImageOnly(info, '{}.img'.format(img), incremental, True)
-    for part in img_map[img]:
-      fw_cmd += 'package_extract_file("{}.img", "/dev/block/by-name/{}");\n'.format(img, part)
+    for img in img_map.keys():
+        AddImageOnly(info, '{}.img'.format(img), incremental, True)
+        for part in img_map[img]:
+            fw_cmd += 'package_extract_file("{}.img", "/dev/block/by-name/{}");\n'.format(
+                img, part
+            )
 
-  for _bin in bin_map.keys():
-    AddImageOnly(info, '{}.bin'.format(_bin), incremental, True)
-    for part in bin_map[_bin]:
-      fw_cmd += 'package_extract_file("{}.bin", "/dev/block/by-name/{}");\n'.format(_bin, part)
+    for _bin in bin_map.keys():
+        AddImageOnly(info, '{}.bin'.format(_bin), incremental, True)
+        for part in bin_map[_bin]:
+            fw_cmd += 'package_extract_file("{}.bin", "/dev/block/by-name/{}");\n'.format(
+                _bin, part
+            )
 
-  info.script.AppendExtra(fw_cmd)
+    info.script.AppendExtra(fw_cmd)

reorder-libs.py
@@ -1,66 +1,0 @@
-#!/usr/bin/env python
-#
-# Copyright (C) 2021 The LineageOS Project
-#
-# SPDX-License-Identifier: Apache-2.0
-#
-
-from functools import cmp_to_key
-from locale import LC_ALL, setlocale, strcoll
-from pathlib import Path
-
-FILES = [Path(file) for file in [
-    "proprietary-files.txt",
-]]
-
-setlocale(LC_ALL, "C")
-
-def strcoll_extract_utils(string1: str, string2: str) -> int:
-    # Skip logic if one of the string if empty
-    if not string1 or not string2:
-        return strcoll(string1, string2)
-
-    # Remove '-' from strings if there,
-    # it is used to indicate a build target
-    string1 = string1.removeprefix('-')
-    string2 = string2.removeprefix('-')
-
-    # If no directories, compare normally
-    if not "/" in string1 and not "/" in string2:
-        return strcoll(string1, string2)
-
-    string1_dir = string1.rsplit("/", 1)[0] + "/"
-    string2_dir = string2.rsplit("/", 1)[0] + "/"
-    if string1_dir == string2_dir:
-        # Same directory, compare normally
-        return strcoll(string1, string2)
-
-    if string1_dir.startswith(string2_dir):
-        # First string dir is a subdirectory of the second one,
-        # return string1 > string2
-        return -1
-
-    if string2_dir.startswith(string1_dir):
-        # Second string dir is a subdirectory of the first one,
-        # return string2 > string1
-        return 1
-
-    # Compare normally
-    return strcoll(string1, string2)
-
-for file in FILES:
-    if not file.is_file():
-        print(f"File {str(file)} not found")
-        continue
-
-    with open(file, 'r') as f:
-        sections = f.read().split("\n\n")
-
-    ordered_sections = []
-    for section in sections:
-        section_list = [line.strip() for line in section.splitlines()]
-        section_list.sort(key=cmp_to_key(strcoll_extract_utils))
-        ordered_sections.append("\n".join(section_list))
-
-    with open(file, 'w') as f:
-        f.write("\n\n".join(ordered_sections).strip() + "\n")