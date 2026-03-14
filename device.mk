06f5ce554a27befd04c2c214c04b3ecd38143655
Debayan Kar <kardebayan3@gmail.com>
2026-03-14 07:28:53 +0000

    salaa: Build MediaTek Gadget Service
    Change-Id: I5f1d9a47f0acf2bb20cd5822dadd62b43437db08

--- device.mk
+++ device.mk
@@ -352,7 +352,8 @@ PRODUCT_PACKAGES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb-service.mediatek
+    android.hardware.usb-service.mediatek \
+    android.hardware.usb.gadget-service.mediatek
 
 # Vibrator
 PRODUCT_PACKAGES += \
