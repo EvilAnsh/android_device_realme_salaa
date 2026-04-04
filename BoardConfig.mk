011c6f2728124c076023b327c99a806b0ae15270
Ansh <singhansh64321@gmail.com>
2026-04-04 11:45:01 +0530

    salaa: Guard reserved partitions
    Change-Id: Iebbf8e2d4eb30fad4f9e497c628263e29eb55540

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -76,6 +76,10 @@ BOARD_CACHEIMAGE_FILE_SYSTEM_TYPE := ext4
 BOARD_USES_METADATA_PARTITION := true
 
 # Partitions (Dynamic)
+ifneq ($(WITH_GMS),true)
+# Reserve space for gapps install in Vanilla builds
+-include vendor/lineage/config/BoardConfigReservedSize.mk
+endif
 BOARD_SUPER_PARTITION_GROUPS := main
 BOARD_MAIN_SIZE := 8048869376
 BOARD_MAIN_PARTITION_LIST := system system_ext vendor product odm
